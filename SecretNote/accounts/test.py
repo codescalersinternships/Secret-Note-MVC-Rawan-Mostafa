30650ac473ca75350e00f69b3f55177454fe8a74
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-10-16 23:22:25 +0300

    test: Adding unit tests for login

--- SecretNote/accounts/test.py
+++ SecretNote/accounts/test.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
+from django.utils import timezone
 class TestSignup(TestCase):
     def test_signup_correctly(self):
         data = {
@@ -12,7 +12,6 @@ class TestSignup(TestCase):
         self.assertRedirects(response, '/accounts/login/')
         user = get_user_model().objects.get(username=data['username'])
         self.assertIsNotNone(user)
-        self.assertTrue(user.is_active)
 
     def test_signup_incorrectly(self):
         data = {
@@ -27,3 +26,26 @@ class TestSignup(TestCase):
             self.fail("Testcase should have failed due to incorrect password confirmation")  
         except get_user_model().DoesNotExist as e:
             self.assertEqual(str(e), "User matching query does not exist.")
+
+class TestLogin(TestCase):
+    def test_login_correctly(self):
+        user = get_user_model().objects.create_user(username='test_user', password='strong_password')
+        data = {
+            'username': 'test_user',
+            'password': 'strong_password'
+        }
+        response = self.client.post('/accounts/login/', data=data,follow=True)
+        self.assertEqual(response.status_code,200)
+        self.assertRedirects(response, '/notes/')
+        self.assertTrue(response.context['user'].is_authenticated)
+
+    def test_login_incorrectly(self):
+        user = get_user_model().objects.create_user(username='test_user', password='strong_password')
+        data = {
+            'username': 'test_user',
+            'password': 'strong_passwors'
+        }
+        response = self.client.post('/accounts/login/', data=data,follow=True)
+        self.assertNotContains(response, 'notes')
+        self.assertFalse(response.context['user'].is_authenticated)
+
