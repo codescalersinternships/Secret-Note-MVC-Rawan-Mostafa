519308e2f583cdfefe5dbb03b09d7462ffb63253
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-10-19 23:50:48 +0300

    test: Integration tests: Adding login testcases

--- SecretNote/tests.py
+++ SecretNote/tests.py
@@ -25,54 +25,73 @@ class EndToEndTests(LiveServerTestCase):
     def setUp(self):
         self.username = f"testinguser_{''.join(random.choices(string.ascii_lowercase,k=8))}"
         self.password = "strong_password"
-    
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+    def signup(self,username,password1,password2):
+        self.driver.get('http://127.0.0.1:8000/accounts/signup')
+
+        username_field=self.driver.find_element(By.ID,"id_username")
+        username_field.send_keys(username)
+
+        password1_field=self.driver.find_element(By.ID,"id_password1")
+        password1_field.send_keys(password1)
+
+        password2_field=self.driver.find_element(By.ID,"id_password2")
+        password2_field.send_keys(password2)
+
+
+        button=self.driver.find_element(By.CLASS_NAME,"submit-btn")
+        button.click()
+
+    def login(self,username,password):
+        self.driver.get('http://127.0.0.1:8000/accounts/login')
+
+        username_field=self.driver.find_element(By.ID,"id_username")
+        username_field.send_keys(username)
+
+        password_field=self.driver.find_element(By.ID,"id_password")
+        password_field.send_keys(password)
+
+
+        button=self.driver.find_element(By.CLASS_NAME,"submit-btn")
+        button.click()
 
     def test_correct_signup(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get('http://127.0.0.1:8000/accounts/signup')
-
-        username=driver.find_element(By.ID,"id_username")
-        username.send_keys(self.username)
-
-        password1=driver.find_element(By.ID,"id_password1")
-        password1.send_keys(self.password)
-
-        password2=driver.find_element(By.ID,"id_password2")
-        password2.send_keys(self.password)
-
-
-        button=driver.find_element(By.CLASS_NAME,"submit-btn")
-        button.click()
-
-        assert "/login" in driver.current_url
-        assert is_element_present_by_id(driver,"login-title") is True
-
-        driver.close()
+        self.signup(self.username,self.password,self.password)
+        assert "/login" in self.driver.current_url
+        assert is_element_present_by_id(self.driver,"login-title") is True
+        self.driver.close()
 
     def test_incorrect_signup(self):
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-        driver.get('http://127.0.0.1:8000/accounts/signup')
+        self.signup(self.username,self.password,"another_password")
+        assert "/login" not in self.driver.current_url
+        assert is_element_present_by_id(self.driver,"error") is True
+        self.driver.close()
 
-        username=driver.find_element(By.ID,"id_username")
-        username.send_keys(self.username)
+    def test_correct_login(self):
+        self.signup(self.username,self.password,self.password)
+        self.login(self.username,self.password)
+        assert "/notes" in self.driver.current_url
+        assert is_element_present_by_id(self.driver,"create-note-btn") is True
 
-        password1=driver.find_element(By.ID,"id_password1")
-        password1.send_keys(self.password)
+        self.driver.close()
 
-        password2=driver.find_element(By.ID,"id_password2")
-        password2.send_keys("another_password")
+    def test_incorrect_username_login(self):
+        self.signup(self.username,self.password,self.password)
+        self.login("another_username",self.password)
 
+        assert "/notes" not in self.driver.current_url
+        assert "/login" in self.driver.current_url
+        assert is_element_present_by_id(self.driver,"create-note-btn") is False
+        self.driver.close()
 
-        button=driver.find_element(By.CLASS_NAME,"submit-btn")
-        button.click()
+    def test_incorrect_password_login(self):
+        self.signup(self.username,self.password,self.password)
+        self.login(self.username,"another_password")
 
-        assert "/login" not in driver.current_url
-        assert is_element_present_by_id(driver,"error") is True
-
-        driver.close()
-
-        
-        
-
+        assert "/notes" not in self.driver.current_url
+        assert "/login" in self.driver.current_url
+        assert is_element_present_by_id(self.driver,"create-note-btn") is False
+        self.driver.close()
 
 
