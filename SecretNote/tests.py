bc83093da89b54d44023691c887a9f2f480e462a
RawanMostafa08 <rawanmostafa401@gmail.com>
2024-10-20 01:22:24 +0300

    test: Integration tests: Adding create note testcases

--- SecretNote/tests.py
+++ SecretNote/tests.py
@@ -4,10 +4,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from django.utils import timezone
 import random
 import string
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def is_element_present_by_id(driver, id):
     try: driver.find_element(By.ID, id)
@@ -16,6 +18,11 @@ def is_element_present_by_id(driver, id):
 
 def is_element_present_by_classname(driver, classname):
     try: driver.find_element(By.CLASS_NAME, classname)
+    except NoSuchElementException as e: return False
+    return True
+
+def is_element_present_by_css(driver, css):
+    try: driver.find_element(By.CSS_SELECTOR, css)
     except NoSuchElementException as e: return False
     return True
 
@@ -56,6 +63,25 @@ class EndToEndTests(LiveServerTestCase):
         button=self.driver.find_element(By.CLASS_NAME,"submit-btn")
         button.click()
 
+    def create_note(self,title,content,date,views):
+        create_note_button=self.driver.find_element(By.ID,"create-note-btn")
+        create_note_button.click()
+
+        title_field=self.driver.find_element(By.ID,"title")
+        title_field.send_keys(title)
+
+        title_field=self.driver.find_element(By.ID,"content")
+        title_field.send_keys(content)
+
+        expiration_field=self.driver.find_element(By.ID,"expiration")
+        expiration_field.send_keys(date)
+
+        max_views_field=self.driver.find_element(By.ID,"max_views")
+        max_views_field.send_keys(views)
+
+        create_button=self.driver.find_element(By.ID,"create-btn")
+        create_button.click()
+
     def test_correct_signup(self):
         self.signup(self.username,self.password,self.password)
         assert "/login" in self.driver.current_url
@@ -94,4 +120,38 @@ class EndToEndTests(LiveServerTestCase):
         assert is_element_present_by_id(self.driver,"create-note-btn") is False
         self.driver.close()
 
+    def test_correct_create_note(self):
+        self.signup(self.username,self.password,self.password)
+        self.login(self.username,self.password)
 
+        assert is_element_present_by_id(self.driver,"no-notes") is True
+
+        self.create_note("test-title","test-content","12/10/2024",2)
+        
+        assert "http://127.0.0.1:8000/notes/" == self.driver.current_url
+        assert is_element_present_by_id(self.driver,"no_notes") is False
+
+        latest_note_title=self.driver.find_elements(By.CLASS_NAME,"title")
+        assert latest_note_title[len(latest_note_title)-1].text == "test-title"
+
+    def test_empty_fields_create_note(self):
+        self.signup(self.username,self.password,self.password)
+        self.login(self.username,self.password)
+
+        assert is_element_present_by_id(self.driver,"no-notes") is True
+
+        self.create_note("","test-content","12/10/2024",2)
+        
+        assert "/notes/create" in self.driver.current_url
+        assert is_element_present_by_classname(self.driver,"errorlist")
+
+    def test_past_date_create_note(self):
+        self.signup(self.username,self.password,self.password)
+        self.login(self.username,self.password)
+
+        assert is_element_present_by_id(self.driver,"no-notes") is True
+
+        self.create_note("test-title","test-content","12/10/2023",2)
+        
+        assert "/notes/create" in self.driver.current_url
+        assert is_element_present_by_classname(self.driver,"errorlist")
